0978af04bd5783f0325072404f19b861ce782824
Santiago Souto <ssout@dolby.com>
2023-06-16 20:19:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import TriviaCard from './TriviaCard';
+import { futbolQuestions, nbaQuestions, uruguayQuestions, musicaQuestions } from './utils/getTriviaQuestions'
 
-function App() {
+
+const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [categoryOptions, setCategoryOptions] = useState([]);
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  useEffect(() => {
+    setCategoryOptions(shuffleArray(['Futbol', 'NBA', 'Historia contemporanea del Uruguay', 'Musica']));
+  }, []);
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const triviaData = [
+    ...futbolQuestions,
+    ...nbaQuestions,
+    ...uruguayQuestions,
+    ...musicaQuestions
+    // Add more trivia questions here...
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <h1 style={styles.header}>Trivia App</h1>
+      <div style={styles.navbar}>
+        {categoryOptions.map((category, index) => (
+          <span
+            key={index}
+            style={category === selectedCategory ? styles.activeNavLink : styles.navLink}
+            onClick={() => handleCategoryChange(category)}
+          >
+            {category}
+          </span>
+        ))}
+      </div>
+      {selectedCategory && (
+        <div style={styles.cardContainer}>
+          {triviaData
+            .filter((trivia) => trivia.category === selectedCategory)
+            .map((trivia, index) => (
+              <TriviaCard
+                key={index}
+                question={trivia.question}
+                options={trivia.options}
+                correctOption={trivia.correctOption}
+              />
+            ))}
+        </div>
+      )}
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontFamily: 'Arial, sans-serif', // Example font family
+  },
+  header: {
+    marginBottom: '16px',
+  },
+  navbar: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: '16px',
+  },
+  navLink: {
+    cursor: 'pointer',
+    marginRight: '8px',
+    padding: '8px',
+    textDecoration: 'none',
+    color: '#333',
+    fontWeight: 'bold',
+    borderRadius: '4px',
+    backgroundColor: '#f5f5f5',
+  },
+  activeNavLink: {
+    cursor: 'pointer',
+    marginRight: '8px',
+    padding: '8px',
+    textDecoration: 'none',
+    color: '#fff',
+    fontWeight: 'bold',
+    borderRadius: '4px',
+    backgroundColor: '#333',
+  },
+  cardContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+};
 
 export default App;
